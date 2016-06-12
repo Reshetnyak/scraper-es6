d804bf77695a2aa52b31ec137a4298f75eaa1ad3
Reshetnyak <reshetnjak@gmail.com>
2016-06-12 21:10:32 +0200

    Parser is finished

--- parser.js
+++ parser.js
@@ -17,23 +17,23 @@ class Parser{
         const property = {
             propertyvendor: this.options.realEstateName,
             reference: this.getReference($),
-            propertytype: getPropertyType($),
-//            propertylocation: getLocation(),
-//            propertydistrict: getDistrict(),
-//            price: getPrice(),
-//            propertytransactiontype: getTransactionType(),
-//            numberofrooms: getNumOfRooms(),
-//            numberofbathrooms: getNumOfBaths(),
-//            floornumber: getFloorNum(),
-//            area: getArea(),
-//            tags: getTags(),
-//            condition: getCondition(),
-//            orientation: getOrientation(),
-//            distancetosea: getDistanceToSea(),
-//            gpslocation: getGpsLocation(),
-//            images: imageArray,
+            propertytype: this.getPropertyType($),
+            propertylocation: this.getLocation($),
+            propertydistrict: this.getDistrict($),
+            price: this.getPrice($),
+            propertytransactiontype: this.getTransactionType($),
+            numberofrooms: this.getNumOfRooms($),
+            numberofbathrooms: this.getNumOfBaths($),
+            floornumber: this.getFloorNum($),
+            area: this.getArea($),
+            tags: this.getTags($),
+            condition: this.getCondition($),
+            orientation: this.getOrientation($),
+            distancetosea: this.getDistanceToSea($),
+            gpslocation: this.getGpsLocation($),
+            images: this.getImages($),
             url: propertylink,
-//            localurl: localUrl
+            localurl: this.getLocalUrl($)
         };
 
         return property;
@@ -56,162 +56,136 @@ class Parser{
             return '';
         }
     }
+    //TODO: change getter to better one
+    getLocation($){
+        try{
+            // original getter
+            return $('.pageH2').text().split(/\ben\b|\bzona\b/).map(str => str.trim())[1];
+            // suggestion: $('.pageH2').text().match(/\ben\s(\S+)/)[1]
+        }catch(e){
+            console.log('error in getting Location', e);
+            return '';
+        }
+    }
+    //TODO: change getter to better one
+    getDistrict($){
+        try{
+            //return $('.pageH2').text().split(/\ben\b|\bzona\b/).map(str => str.trim())[2];
+            //suggestion:
+            return $('.pageH2').text().match(/zona\s*(.+)/)[1].trim();
+        }catch(e){
+            console.log('error in getting District', e);
+            return '';
+        }
+    }
+
+    getPrice($){
+        try{
+            // original return $('.titulo-vivienda').text().match(/\d+\.\d+/, '')[0];
+            return $('.titulo-vivienda').text().match(/\d+\.?\d+\.?\d+/g)[0].replace(/\./, ''); // check is needed
+        } catch(e){
+            console.log('error in getting Price', e);
+            return '';
+        }
+    }
+
+    getTransactionType($){
+        // original return $('body > div:nth-child(3) > div.row.hidden-print > div > ol > li:nth-child(2) > a').text().trim();
+        return $('ol.breadcrumb > li:nth-child(2) > a').text().trim(); // check is needed
+    }
+
+    getNumOfRooms($){
+        try {
+            return parseInt($('.alert-warning').text().replace(/\D+/g, ''), 10) || 0;
+        } catch(e){
+            console.log('error in getting Rooms', e);
+            return 0;
+        }
+    }
+
+    getNumOfBaths($){
+        try{
+            return parseInt( $('.alert-info').text().replace(/\D+/g, ''), 10) || 0;
+        }catch(e){
+            console.log('error in getting Bathrooms', e);
+            return 0;
+        }
+    }
+
+    getFloorNum($){
+        try {
+            return parseInt($('li:contains("Planta")').text().replace(/\D+/g, ''), 10) || 0;
+        }catch(e){
+            console.log('error in getting Floornum', e);
+            return 0;
+        }
+    }
+
+    getArea($){
+        return parseInt( $('.alert-metros-cuadrados').text().replace(/\D+/g, ''), 10);
+    }
+
+    getTags($){
+        try{
+            const tags = $('.bordeAmarillo:not(".text-left")').first()[0].next.data.replace(/^\s*|\.\s*$/g, '');
+            // console.log(tags.split(/\,/).map(function(elem){ return elem.trim()}));
+            return  tags ? tags.split(/\,/).map( elem => elem.trim() ) : [] ;
+        }catch(e){
+            console.log('error in getTags', e);
+            return [];
+        }
+    }
+
+    getCondition($){
+        try{
+            return $('strong:contains("Estado")')[0].next.data.replace(/^\s*|\s*$/g, '');
+        }catch(e){
+            console.log('error in get conditions', e);
+            return '';
+        }
+    }
+
+    getOrientation($){
+        try{
+            return $('strong:contains("Orientación")')[0].next.data.replace(/^\s*|\s*$/g, '');
+        }catch(e){
+            console.log('error in get orientation', e);
+            return '';
+        }
+    }
+
+    getDistanceToSea($){
+        try{
+            return parseInt( $('strong:contains("Distancia al mar")')[0].next.data.match(/\d+/g), 10) || 0;
+        }catch(e){
+            console.log('error in get getDistanceToSea', e);
+            return 0;
+        }
+    }
+
+    getGpsLocation($){
+        try{
+            return $('h3:contains("Localización")').next().attr('src').match(/(?:(?!\|))\d+.*?(?=&)/g)[0];
+        }catch(e){
+            console.log('error in get getGpsLocation', e);
+            return '';
+        }
+    }
+
+    getImages($){
+        return $('#galeria-thumbs a').map( (i, link) => $(link).attr('href') ).toArray();
+    }
+
+    getLocalUrl($){
+
+        const propType = this.getPropertyType($);
+        const location = this.getLocation($);
+        const numOfRooms = this.getNumOfRooms($);
+        const reference  = this.getReference($);
+        const realEstateName = this.realEstateName;
+
+        return `${propType}-en-${location}-con-${numOfRooms}-habitaciones-${realEstateName}-${reference}.html`;
+    }
 }
 
 module.exports = Parser;
-/*
-parseOffer(body, offerLink){
-
-        var $ = cheerio.load(body);
-
-        //console.log($('body').html());
-
-        var realEstateName = 'Visual-Home';
-
-
-        var
-
-        var getLocation  = () => {
-            try{
-                return $('.pageH2').text().split(/\ben\b|\bzona\b/).map(str => str.trim())[1];
-            }catch(e){
-                console.log('error in getting Location', e);
-            }
-            return '';
-        };
-
-        var getDistrict  = () => {
-            try{
-                return $('.pageH2').text().split(/\ben\b|\bzona\b/).map(str => str.trim())[2];
-            }catch(e){
-                console.log('error in getting District', e);
-                return '';
-            }
-            return '';
-        };
-
-        var getPrice      = () => ($('.titulo-vivienda').text().match(/\d+\.\d+/, '') || [''] )[0];
-
-        var getTransactionType      = () => $('body > div:nth-child(3) > div.row.hidden-print > div > ol > li:nth-child(2) > a').text().trim();
-
-        var getNumOfRooms = () => {
-          try {
-            return parseInt($('.alert-warning').text().replace(/\D+/g, ''), 10) || 0;
-          } catch(e){
-            console.log('error in getting Rooms', e);
-            //return 0;
-          }
-          return 0;
-        };
-
-        var getNumOfBaths = () => {
-          try{
-            return parseInt( $('.alert-info').text().replace(/\D+/g, ''), 10) || 0;
-          }catch(e){
-            console.log('error in getting Bathrooms', e);
-            return 0;
-          }
-            return 0;
-        };
-
-        var getFloorNum = () => {
-          try {
-            return parseInt($('li:contains("Planta")').text().replace(/\D+/g, ''), 10) || 0;
-          }catch(e){
-            console.log('error in getting Floornum', e);
-            return 0;
-          }
-            return 0;
-        };
-
-        var getArea = () => parseInt($('.alert-metros-cuadrados').text().replace(/\D+/g, ''));
-
-        var getTags = () => {
-            try{
-              var tags = $('.bordeAmarillo:not(".text-left")').first()[0].next.data.replace(/^\s*|\.\s*$/g, '');
-                // console.log(tags.split(/\,/).map(function(elem){ return elem.trim()}));
-                return  tags ? tags.split(/\,/).map(function(elem){ return elem.trim()}) : [] ;
-            }catch(e){
-                console.log('error in getTags', e);
-                return [];
-            }
-
-            return '';
-        };
-
-
-        var getCondition = () => {
-            try{
-                return $('strong:contains("Estado")')[0].next.data.replace(/^\s*|\s*$/g, '');
-            }catch(e){
-                console.log('error in get conditions', e);
-                return '';
-            }
-
-            return '';
-        };
-
-        var getOrientation = () => {
-            try{
-                return $('strong:contains("Orientación")')[0].next.data.replace(/^\s*|\s*$/g, '');
-            }catch(e){
-                console.log('error in get orientation', e);
-                return '';
-            }
-
-            return '';
-        };
-
-        var getDistanceToSea = () => {
-            try{
-                return parseInt($('strong:contains("Distancia al mar")')[0].next.data.match(/\d+/g), 10) || 0;
-            }catch(e){
-                console.log('error in get getDistanceToSea', e);
-                return 0;
-            }
-
-            return '';
-        };
-
-        var getGpsLocation = () => {
-            try{
-                return $('h3:contains("Localización")').next().attr('src').match(/(?:(?!\|))\d+.*?(?=&)/g)[0];
-            }catch(e){
-                console.log('error in get getGpsLocation', e);
-                return '';
-            }
-
-            return '';
-        };
-
-        var imageArray = [];
-
-        var getImages = $('#galeria-thumbs a').each((i, link) => imageArray.push( $(link).attr('href') ));//.map((i, link) => $(link).attr('href') );
-
-        var localUrl = getPropertyType() + '-en-' + getLocation() + '-con-' + getNumOfRooms() + '-habitaciones-' + realEstateName + '-' + getReference() + '.html';
-
-        var property = {
-            propertyvendor: realEstateName,
-            reference: getReference(),
-            propertytype: getPropertyType(),
-            propertylocation: getLocation(),
-            propertydistrict: getDistrict(),
-            price: getPrice(),
-            propertytransactiontype: getTransactionType(),
-            numberofrooms: getNumOfRooms(),
-            numberofbathrooms: getNumOfBaths(),
-            floornumber: getFloorNum(),
-            area: getArea(),
-            tags: getTags(),
-            condition: getCondition(),
-            orientation: getOrientation(),
-            distancetosea: getDistanceToSea(),
-            gpslocation: getGpsLocation(),
-            images: imageArray,
-            url: offerLink,
-            localurl: localUrl
-        };
-
-        return property;
-    }
-    */
